f02c725fd207c8fe417a168471b8b8c34e359b04
Jason <jasongiovany@gmail.com>
2022-10-26 17:01:01 -0600

    addEventListener modification

--- Script.js
+++ Script.js
@@ -1,19 +1,31 @@
 const cantidadPesos = document.querySelector("#pesos");
-const btn = document.querySelector("calcularButton");
+const btn = document.querySelector("#calcularButton");
+const pResult = document.querySelector("#result");
+
+const dolas = document.querySelector("#usdinput");
+const btnUsd = document.querySelector("#usdbtn");
+const UsdResult = document.querySelector("#resultUSD");
+
 const cambio = 0.42;
 const cambioUsd = 8.10;
-const pResult = document.querySelector("#result")
-const clear = document.querySelector("#clear")
-const clearBtn = document.querySelector("#clear")
-const UsdResult = document.querySelector("#resultUSD")
-const dolas = document.querySelector("#usdinput")
 
-function conversionPesos(){
-    const convertion = parseFloat(cantidadPesos.value) * cambio;
-    pResult.innerText = convertion + " Quetzales";
+
+if(btn){
+    btn.addEventListener("click", conversionPesos);
 }
 
-function conversionUsd(){
+if(btnUsd){
+    btnUsd.addEventListener("click", conversionUsd);
+}
+
+function conversionPesos() {
+    const convertion = parseFloat(cantidadPesos.value) * cambio;
+    pResult.innerText = convertion + " Quetzales";
+    console.log(convertion);
+}
+
+function conversionUsd() {
     const convertion = parseFloat(dolas.value) * cambioUsd;
     UsdResult.innerText = convertion + " Quetzales";
 }
+
