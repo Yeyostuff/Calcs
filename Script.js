181462362e9d5161b4398ce401d9878fa201151c
Yeyostuff <jasongiovany@gmail.com>
2022-11-17 20:35:11 -0600

    New changes, Forza Delivery Calcs

--- Script.js
+++ Script.js
@@ -1,31 +1,20 @@
 const cantidadPesos = document.querySelector("#pesos");
 const btn = document.querySelector("#calcularButton");
 const pResult = document.querySelector("#result");
+const dResult = document.querySelector('#deposito')
 
-const dolas = document.querySelector("#usdinput");
-const btnUsd = document.querySelector("#usdbtn");
-const UsdResult = document.querySelector("#resultUSD");
-
-const cambio = 0.42;
-const cambioUsd = 8.10;
-
+const envioForza = 28;
+const comision = .035;
 
 if(btn){
     btn.addEventListener("click", conversionPesos);
 }
 
-if(btnUsd){
-    btnUsd.addEventListener("click", conversionUsd);
-}
 
 function conversionPesos() {
-    const convertion = parseFloat(cantidadPesos.value) * cambio;
-    pResult.innerText = convertion + " Quetzales";
-    console.log(convertion);
+    const menosEnvio = cantidadPesos.value - envioForza;
+    pResult.innerText = menosEnvio + " Quetzales";
+    const aDepositar = menosEnvio * comision;
+    const totalTotal = menosEnvio - aDepositar;
+    dResult.innerText = totalTotal + ' Quetzales';
 }
-
-function conversionUsd() {
-    const convertion = parseFloat(dolas.value) * cambioUsd;
-    UsdResult.innerText = convertion + " Quetzales";
-}
-
